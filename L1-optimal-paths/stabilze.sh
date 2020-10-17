edcb03dc5c8f64a5c966b122fe63d22bed0abbe9
ishank-juneja <ishankjuneja@gmail.com>
2020-10-17 23:52:04 +0530

    Bash script excludes pre-stabilzed files while processing, L1 optimal optimized a bit
    Former-commit-id: d7e2a8957d16282bc0477552d3c0a3ad369cbcf7

--- L1-optimal-paths/stabilze.sh
+++ L1-optimal-paths/stabilze.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-for instance in /home/ishank/Desktop/Regular/*.avi; do
-	# Checks if a file like instance
-	# actually exists or is just a glob	
-	[ -e "$instance" ] || continue
-	# Plot arm functions
-	echo -e $"--------------------------------------------\n"
-	echo -e $"Stabilizing file $instance\n"
-	python3 stabilization_L1_optimal.py -i "$instance"
+for unstable_video in /home/ishank/courses_IITB/unstable_video_data_Regular/*.avi; do
+    # Checks if a file like unstable_video
+    # actually exists or is just a glob	in itself
+    [ -e "$unstable_video" ] || continue
+    # Check if it is already a stabilized video containing
+    # the keyword 'stb' inside it
+    if [[ $unstable_video == *"stb"* ]]; then
+        continue
+    else
+        # Plot arm functions
+        echo -e $"--------------------------------------------\n"
+        echo -e $"Stabilizing file $unstable_video\n"
+        python3 stabilization_L1_optimal.py -i "$unstable_video" -crop-ratio 0.7
+    fi
 done
